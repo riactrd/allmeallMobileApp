38773d365e12c0086ce0e955d9127ded38c0c644
eurorondon03 <eurorondon03@gmail.com>
2023-09-30 18:10:51 -0400

    sending data from the form, you just need to bring the store data, name, email and phone

--- src/redux/api/ContactApi.ts
+++ src/redux/api/ContactApi.ts
@@ -1,49 +1,57 @@
-import { BaseQueryFn, createApi, FetchArgs, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "@reduxjs/toolkit/dist/query/core/apiState";import { ContactModel, ResponseContactModel } from "../../model/ContactModel";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
+import { RootState } from "@reduxjs/toolkit/dist/query/core/apiState";
+import { ContactModel, ResponseContactModel } from "../../model/ContactModel";
 import { URL_VAR } from "@env";
 
-interface errorModel{
-  data:{
-      code: number,
-      error:{
-        message: string,
-      },
-      success:boolean,
-  }
+interface errorModel {
+  data: {
+    code: number;
+    error: {
+      message: string;
+    };
+    success: boolean;
+  };
 }
 
-
 export const contactApi = createApi({
-  
   reducerPath: "contactApi",
-  baseQuery: fetchBaseQuery({ baseUrl: URL_VAR, 
-  prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).login.token
+  baseQuery: fetchBaseQuery({
+    baseUrl: URL_VAR,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).login.token;
 
-    // If we have a token set in state, let's assume that we should be passing it.
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`)
-    }
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
 
-    return headers
-  },
-}) as BaseQueryFn <string | FetchArgs, unknown, errorModel, {}>,
+      return headers;
+    },
+  }) as BaseQueryFn<string | FetchArgs, unknown, errorModel, {}>,
   endpoints: (builder) => ({
-   
     sentcontact: builder.mutation<ResponseContactModel, ContactModel>({
-      query: contact =>({
-          url: 'contact-us',
-          method: 'POST',
-          body: contact
-      })
+      query: (contact) => ({
+        url: "contact-us",
+        method: "POST",
+        body: {
+          contact_us: {
+            name: "contact.name",
+            email: "contact.email",
+            phone_number: "contact.phone_number",
+            subject: contact.subject,
+            message: contact.message,
+          },
+        },
+      }),
     }),
-    
-
   }),
 });
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { 
-    useSentcontactMutation,
-    } = contactApi;
+export const { useSentcontactMutation } = contactApi;
