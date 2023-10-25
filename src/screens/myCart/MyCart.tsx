dfe76d0c8a0abfaa7f94ae0a6305aa79f8f16527
eurorondon03 <eurorondon03@gmail.com>
2023-10-25 09:24:05 -0400

    mejora contador

--- src/screens/myCart/MyCart.tsx
+++ src/screens/myCart/MyCart.tsx
@@ -50,6 +50,7 @@ import {
   useDeleteItemCartMutation,
   useGetmyCartQuery,
   useIncreaseCartMutation,
+  useLazyGetmyCartQuery,
 } from "../../redux/api/myCartApi";
 import MyAdresses from "../myCart/MyAdresses";
 import MycartItem from "../categoryPage/mealItem/MycartItem";
@@ -83,8 +84,17 @@ const MyCart = () => {
   const itemMeals = useSelector(selectcartItems);
   // const totalQuantity = useSelector(selectcartTotalQuantity);
   // const totalAmount = useSelector(selectTotalAmount);
+
+  //------------------------------------------------------------------
   const { data, isError, error, isLoading, refetch, isFetching } =
     useGetmyCartQuery("bulbasaur", { refetchOnMountOrArgChange: true });
+
+  //--------------------------------------------------------------------
+  const [trigger, { data: dataMycart }] = useLazyGetmyCartQuery({
+    refetchOnFocus: true,
+  });
+
+  //---------------------------------------------------------------------
   const [myItem, SetMyItem] = useState<CartItems[]>([]);
   const [updateItem, SetupdateItem] = useState({
     id: "",
@@ -96,6 +106,10 @@ const MyCart = () => {
     useDecreaseCartMutation();
   const [DeleteIteCart, { isSuccess: deleteSuccess }] =
     useDeleteItemCartMutation();
+
+  useEffect(() => {
+    trigger("");
+  }, [increseSuccess, descreseSuccess]);
 
   const [updateCart, SetupdateCart] = useState<UpdateMyCart>({
     is_prime_membership: isMembership?.checked.toString(),
@@ -200,8 +214,18 @@ const MyCart = () => {
       } catch (error) {
         console.log(error);
       }
+    } else if (dataMycart) {
+      SetMyItem(dataMycart?.data?.my_cart?.cart_items);
     }
-  }, [data, error, isFocused, increseSuccess, descreseSuccess, deleteSuccess]);
+  }, [
+    data,
+    error,
+    isFocused,
+    increseSuccess,
+    descreseSuccess,
+    deleteSuccess,
+    dataMycart,
+  ]);
 
   const onChangeGlasswares = (): void => {
     // const value = e.nativeEvent.text;
